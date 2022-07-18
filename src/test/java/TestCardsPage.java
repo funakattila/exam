0191864ff7fc1cf35d095f7ad0d1fd2ece824dbe
funak <funak.attila@gmail.com>
2022-07-18 20:42:48 +0200

    add description

--- src/test/java/TestCardsPage.java
+++ src/test/java/TestCardsPage.java
@@ -1,9 +1,11 @@
+import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCardsPage extends TestBase{
 
     @Test
+    @Description("test the name on the second card")
     public void testName() {
         CardsPage cardsPage = new CardsPage(driver, wait);
 
